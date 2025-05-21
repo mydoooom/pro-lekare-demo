2b6936264db5402dff5e7eff30a4d802200b4abd
Tran Viet Anh <tran@vietanh.cz>
2025-05-21 09:18:57 +0200

    Refactor articles page and enhance article display.
    Replaced react-query with direct API calls for article fetching, simplifying the code. Updated article components with improved structure, added tags and translations handling, and introduced semantic styling changes. Enhanced global styles with new font choices.

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,23 +1,14 @@
 import { ArticlesList } from '@/components/articles'
 import { ContentsQuery } from '@/graphql/graphql'
-import { getQueryClient } from '@/utils/getQueryClient'
 import request from '@/graphql/request'
-import {ArticlesQuery} from '@/api/queries/ArticlesQuery'
-import { dehydrate, HydrationBoundary } from '@tanstack/react-query'
+import { ArticlesQuery } from '@/api/queries/ArticlesQuery'
 
 export default async function ArticlesPage () {
-  const queryClient = getQueryClient()
-
-  await queryClient.prefetchQuery({
-    queryKey:['articles'],
-    queryFn: () => request<ContentsQuery>(ArticlesQuery)
-  })
+  const articles = await request<ContentsQuery>(ArticlesQuery)
 
   return (
     <>
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <ArticlesList/>
-      </HydrationBoundary>
+      <ArticlesList articles={articles}/>
     </>
   )
 }
